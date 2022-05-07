a74490a7c44802507d201477abf6256d4b6434c6
Akio Olvera <akiooarao@gmail.com>
2022-05-07 10:18:28 -0600

    Adding Feature highlights

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './header/Header';
-import Showcase from './showcase/Showcase'
+import Showcase from './showcase/Showcase';
+import FeatureHighlights from './feature-highlights/FeatureHighlights';
+import { Container } from 'react-bootstrap';
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
-      <Showcase />
+      <Container>
+        <Showcase />
+        <FeatureHighlights />
+      </Container>
     </div>
   );
 }
