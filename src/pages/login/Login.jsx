04926ca85b67f1b1fadde45bd586e86ffb522979
Akio Olvera <akiooarao@gmail.com>
2022-05-08 19:03:29 -0600

    Adding Login form logic

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
 import { Button } from 'react-bootstrap';
 import Header from '../landing-page/header/Header';
+import { validateEmail } from '../../util/email';
 import styles from './Login.module.scss';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState();
+  const [password, setPassword] = useState('');
+
+  const onEmailChange = ({ target: { value } }) => {
+    setEmail(value);
+  }
+
+  const onPasswordChange = ({ target: { value } }) => {
+    setPassword(value);
+  }
+
+  const onButtonClicked = () => {
+    setEmailError();
+    if (!validateEmail(email)) {
+      setEmailError('Por favor ingresa un email valido.');
+      return;
+    }
+    console.log(`Email: ${email}, Password: ${password}`);
+  }
+
   return (
     <div>
       <Header />
@@ -15,14 +37,20 @@ const Login = () => {
           label="Email"
           type="email"
           className={styles.input}
+          onChange={onEmailChange}
+          value={email}
+          error={!!emailError}
+          helperText={emailError}
         />
         <TextField
           variant="standard"
           label="Contraseña"
           type="password"
           className={styles.input}
+          onChange={onPasswordChange}
+          value={password}
         />
-        <Button>
+        <Button onClick={onButtonClicked}>
           Ingresa
         </Button>
         <p className={styles['sign-up']}>
